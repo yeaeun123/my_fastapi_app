e211d4b8f033c76b572a48cff32bcfd3ae8975cc
yeaeun123 <yeaeun0125@gmail.com>
2024-09-04 11:44:19 +0900

    Update params.py

--- routes/params.py
+++ routes/params.py
@@ -13,7 +13,8 @@ router = APIRouter(tags=["params"])
 @router.get("/items/",
             description="단일 쿼리 파라미터")
 async def read_items(item_id: int):
-    return { "item_id": item_id }
+    return {"item_id": item_id}
+
 
 # 여러 쿼리 파라미터를 받는 경우
 # /users/?age=29&name=홍길동
@@ -21,13 +22,22 @@ async def read_items(item_id: int):
 @router.get("/users/",
             description="여러 개의 쿼리 파라미터")
 async def read_users(age: int,
-                     name: Optional[str] =None):
-    return { "name": name, "age": age}
+                     name: Optional[str] = None):
+    return {"name": name, "age": age}
+
 
 # 쿼리 파라미터로 리스트를 받는 경우
 # checkbox
 # /items/multi/?item=아이템1&item=아이템2...
 @router.get("/items/multi/",
             description="쿼리 파라미터로 리스트 받기")
-async def read_items_multi(item: Optional[List[str]]= Query(None)):
+async def read_items_multi(item: Optional[List[str]] = Query(None)):
     return {"items": item}
+
+
+# 필수 쿼리 파라미터
+# /search/?keyword=
+# ... : Ellipse -> 필수 파라미터 
+@router.get("/search/")
+async def search_item(keyword:str = Query(..., min_length=3)):
+    return {"keyword": keyword }
